d40f73f439489bab7d4b14a4cadb4d7df029fb83
alkor63 <alkor@medi-spa.ru>
2024-02-04 01:08:46 +0300

    Добавил чтение шаблонов и текста из файлов и запись результата в файл

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // Задача приложения найти и вывести в файл статистику сколько слов в тексте соответствует шаблонам
         // Шаблоны получаем из внешнего файла
         // Текст для анализа тоже получаем из внешнего файла
@@ -19,15 +23,9 @@ public class Main {
 - когда текст закончится - открываем/создаем файл для результата и записываем туда содержимое мапы шаблонов, закрываем файл
  */
 
-        String inputPatterns = "о2\n" +
-                "т1о1\n" +
-                "\"ми\"\n" +
-                "ф2\n" +
-                "11\n" +
-                "\"йо\"\n" +
-                "й1о1";
+        String inputPatterns = readFileAsString(
+                "D:\\TestPatterns.txt");
 
-//                String inputText = removePunctuations(inputTextWithChar);//удаляем знаки пунктуации
         System.out.println("Шаблоны:\n" + inputPatterns);
         Map<String, Integer> patternsMap = new LinkedHashMap<>();
         Scanner scan = new Scanner(inputPatterns);
@@ -41,19 +39,19 @@ public class Main {
         for (HashMap.Entry<String, Integer> anyPattern : patternsMap.entrySet()) {
             System.out.println("   " + anyPattern.getKey() + "\t " + anyPattern.getValue());
         }
-
         // Читаем слова из входного текста
-        String inputText = "Отец мой Андрей Петрович Гринев в молодости своей служил " +
-                "при графе Минихе и вышел в отставку премьер-майором в 17.. году.";
+        String inputText = readFileAsString(
+                "D:\\TestText.txt");
+//                "Отец мой Андрей Петрович Гринев в молодости своей служил " +
+//                "при графе Минихе и вышел в отставку премьер-майором в 17.. году.";
         scan = new Scanner(inputText);
         while (scan.hasNext()) {
             String word = scan.next(); //scanner automatically uses " " as a delimiter
-            System.out.println("current word = "+word);
             for (HashMap.Entry<String, Integer> anyPattern : patternsMap.entrySet()) {
                 String pattern = anyPattern.getKey();
                 char[] charArray = pattern.toCharArray();
                 if (charArray[0] == '"') {
-                    if (checkString(word, pattern.substring(1,charArray.length-1))) { // true если текущий шаблон обнаружен
+                    if (checkString(word, pattern.substring(1, charArray.length - 1))) { // true если текущий шаблон обнаружен
                         int count = patternsMap.get(pattern) + 1; // увеличиваем счетчик текущего шаблона на 1
                         patternsMap.put(pattern, count); //записываем шаблон с новым значением счетчика
                     }
@@ -74,22 +72,33 @@ public class Main {
         }
         System.out.println("Частотное распределение шаблонов в тексте :");
         System.out.println("шаблон \t количество ");
-        for (HashMap.Entry<String, Integer> anyPattern : patternsMap.entrySet()) {
-            System.out.println("   " + anyPattern.getKey() + "\t " + anyPattern.getValue());
+        try {
+            FileWriter fWriter = new FileWriter(
+                    "D:/testResult.txt");
+
+            fWriter.write("шаблон \t количество \n"); // "шапка"
+
+            for (HashMap.Entry<String, Integer> anyPattern : patternsMap.entrySet()) {
+                fWriter.write(anyPattern.getKey() + "\t " + anyPattern.getValue() + "\n");
+                System.out.println("  " + anyPattern.getKey() + "\t " + anyPattern.getValue());
+            }
+            fWriter.close();
+            System.out.println("Запись информации в файл прошла успешно");
+        } catch (IOException e) {
+            System.out.print(e.getMessage());
         }
     }
-// проверяем наличие подстроки в слове
-            public static boolean checkString(String word, String pattern) {
-                    return word.contains(pattern);
-                }
-
+    // проверяем наличие подстроки в слове
+    public static boolean checkString(String word, String pattern) {
+        return word.contains(pattern);
+    }
     // проверяем наличие символа в слове нужное количество раз
     public static boolean checkChar(String word, char ch, int num) {
         return num == word.toLowerCase().chars().filter(c -> c == ch).count();// ? В ТЗ про Регистр ничего не сказано
     }
-
-            public static boolean nullString(String s) {
-                return (s == null || s.isEmpty() || s.isBlank());
-            }
-        }
+    public static String readFileAsString(String fileName)
+            throws Exception {
+        return new String(Files.readAllBytes(Paths.get(fileName)));
+    }
+}
 
